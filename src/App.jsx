f63947f32ccf1038779cf6b6cf9d0a5fec88c02c
PhuPhuoc <blessforwork@gmail.com>
2023-09-28 14:26:00 +0700

    phuoc commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
-import DefaultLayout from './layout/DefaultLayout'
-// import Login from './pages/DangNhap/Login'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import DefaultLayout from './layout/DefaultLayout';
 
 function App() {
   return (
-    <>
-      {/* <Login></Login> */}
-      <DefaultLayout></DefaultLayout>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<DefaultLayout />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
