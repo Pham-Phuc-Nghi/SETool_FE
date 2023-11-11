095d8fa464e13904ae2dbd00d53458bf93d0478c
RedQW <danhtang226@gmail.com>
2023-11-11 10:53:01 +0700

    Danh commit

--- src/pages/BackLogs/AddBacklogs.jsx
+++ src/pages/BackLogs/AddBacklogs.jsx
@@ -16,13 +16,11 @@ const { Text } = Typography;
 import PropTypes from "prop-types";
 import {
   ArrowDownOutlined,
-  InboxOutlined,
   IssuesCloseOutlined,
   PlusCircleOutlined,
   RiseOutlined,
 } from "@ant-design/icons";
 import "dayjs/locale/vi";
-import Dragger from "antd/es/upload/Dragger";
 import { useDispatch } from "react-redux";
 import { createBacklogs } from "../../Redux/Slices/Backlogs/BacklogsSlice";
 
@@ -45,27 +43,6 @@ const AddBacklogs = ({ onClose, form }) => {
     onClose();
   };
 
-  const props = {
-    name: "file",
-    multiple: true,
-    accept: ".png,.jpg",
-    action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
-    onChange(info) {
-      const { status } = info.file;
-      if (status !== "uploading") {
-        // console.log(info.file, info.fileList);
-      }
-      if (status === "done") {
-        message.success(`${info.file.name} file uploaded successfully.`);
-      } else if (status === "error") {
-        message.error(`${info.file.name} file upload failed.`);
-      }
-    },
-    onDrop(e) {
-      console.log("Dropped files", e.dataTransfer.files);
-    },
-  };
-
   const openNotification = (type, message) => {
     notification.error({
       message,
@@ -73,8 +50,17 @@ const AddBacklogs = ({ onClose, form }) => {
     });
   };
 
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+  };
+
   const handleCreateBacklogs = (values) => {
     const data = { ...values };
+    data.imgLink = file;
+    console.log(data);
     if (data) {
       dispatch(createBacklogs(data))
         .unwrap()
@@ -199,24 +185,12 @@ const AddBacklogs = ({ onClose, form }) => {
           ></Input.TextArea>
         </Form.Item>
         <Form.Item
-          label={<Text>imgLink</Text>}
+          label={<Text>Image Description</Text>}
           name="imgLink"
           style={{ marginRight: 10 }}
         >
-          <Dragger {...props}>
-            <p className="ant-upload-drag-icon">
-              <InboxOutlined />
-            </p>
-            <p className="ant-upload-text">
-              Click or drag file to this area to upload
-            </p>
-            <p className="ant-upload-hint">
-              Support for a single or bulk upload. Strictly prohibited from
-              uploading company data or other banned files.
-            </p>
-          </Dragger>
+          <input type="file" onChange={handleFileChange} />
         </Form.Item>
-
         <Row gutter={24} style={{ marginRight: "5px" }}>
           <Col
             span={24}
